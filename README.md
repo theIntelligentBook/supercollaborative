0126240fed75a51032f5f7c0063e5e0498899856
William Billingsley <wbillingsley@cantab.net>
2022-05-27 13:16:26 +1000

    Initial skeleton of a mostly empty book

---
+++ README.md
@@ -0,0 +1,3 @@
+# Supercollaborative, Build: How teams of teams build software
+
+This will contain materials on collaborative software engineering, continuous deployment, etc.
---
+++ src/main/scala/supercollaborative/Main.scala
@@ -0,0 +1,81 @@
+package willscala
+
+import com.wbillingsley.veautiful.html._
+import com.wbillingsley.veautiful.doctacular._
+import Medium._
+import org.scalajs.dom
+
+import Common._
+import templates.{markdownDeck, markdownPage, markdownChapterPage, Example, OutsideExample, GitTutorial}
+import templates.given
+
+val site = Site()
+
+
+object Main {
+
+  val scaleChallengesToWindow:Boolean = {
+    !dom.window.location.search.contains("scale=off")
+  }
+
+  def main(args:Array[String]): Unit = {
+    val n = dom.document.getElementById("render-here")
+    n.innerHTML = ""
+
+    Styles.installStyles()
+
+    import site.given 
+        
+    site.toc = site.Toc(
+      "Home" -> site.HomeRoute,
+      
+      "1. Version Control" -> site.Toc(
+      ),
+
+      "2. Containers and Virtual Machines" -> site.Toc(
+
+      ),
+
+      "3. Build Systems" -> site.Toc(
+
+      ),
+
+      "4. Testing" -> site.Toc(
+
+      ),
+
+      "5. Continuous Development" -> site.Toc(
+
+      ),
+
+      "6. System Architecture" -> site.Toc(
+
+      ),
+
+      "7. Models and Diagrams" -> site.Toc(
+
+      ),
+
+      "8. Logging and Debugging" -> site.Toc(
+
+      ),
+
+      "9. Deployment and Ops" -> site.Toc(
+
+      ),
+
+      "10. Social Implications" -> site.Toc(
+
+      ),
+
+      "11. User Centred Design" -> site.Toc(
+
+      ),
+    )
+    
+    site.home = () => site.renderPage(frontPage)
+    site.attachTo(n)
+
+  }
+
+}
